87a801d39ad9c207cd49021165e897b0df11e93e
richardysonnascimento <richardysonnascimento@gmail.com>
2026-05-13 20:46:24 -0300

    Fix variable declaration and loop index in search

--- atividade 6.c
+++ atividade 6.c
@@ -3,15 +3,15 @@
 
 int main() {
     int numeros[10];
-    int pos, valor = 0, encontrou = 0;
-    for (pos = 0; pos < 10; pos++){
-        printf("Digite o número da posição %d:", pos);
+    int i, valor = 0, encontrou = 0;
+    for (i = 0; i < 10; i++){
+        printf("Digite o número da posição %d:", i);
         scanf("%d", &numeros[pos]);
     }
     printf("Digite o valor a ser buscado: ");
     scanf("%d", &valor);
-    for (pos = 0; pos < 10; pos++){
-        if (numeros[pos] == valor){
+    for (pos = 0; i < 10; i++){
+        if (numeros[i] == valor){
             encontrou = 1;
             
         }
